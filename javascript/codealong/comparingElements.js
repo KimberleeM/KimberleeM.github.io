011c0befff82c5d1dd06a7fe76d0c26201976ada
Kimberlee.Mendoza@Gmail.com <Kimberlee.Mendoza@Gmail.com>
2020-04-08 22:28:09 -0400

    updated comparing elements

---
+++ javascript/codealong/comparingElements.js
@@ -0,0 +1,17 @@
+
+let testArray = [0, 1, 1, "1", 3, "311"];
+for (let position = 0; position < testArray.length - 1; position++){
+    let currentElement = testArray[position];
+    let nextElement = testArray[position + 1];
+console.log("Testing " + currentElement + " and " + nextElement + "(greater than): " + (currentElement > nextElement));
+console.log("Testing " + currentElement + " and " + nextElement + "(less than or equal to): " + (currentElement <= nextElement));
+if (currentElement == nextElement) {
+    console.log("Testing " + currentElement + " and " + nextElement + "(strictly equal to): " + (currentElement === nextElement));
+    if (currentElement !== nextElement) {
+        console.log(currentElement + " is " + typeof currentElement);
+        console.log(nextElement + " is " + typeof nextElement);
+      }
+    } else {
+        console.log("Testing " + currentElement + " and " + nextElement + "(equal to): false");
+    }
+}
